f2ef73a2f4a45cb65fb58457ba991eae7e7beabd
Nevojt <nevojt@gmail.com>
2025-01-03 15:41:23 +0100

    good dog

--- dog.go
+++ dog.go
@@ -6,4 +6,8 @@ func WhenGrowUp(s string) string {
 	return "When the puppy grows up it says : " + strings.ToUpper(s)
 }
 
+func GoodBoy(s string) string {
+	return "Good boy, " + s + "!"
+}
+
 // When grow up dog
